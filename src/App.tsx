18af8fd180cb7db0a81cef86859699f6800d40e8
Mikko Korpela <mikko.korpela@gmail.com>
2020-03-21 22:31:11 +0200

    Speeddial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./App.css";
 import GoogleMapReact from "google-map-react";
+import { makeStyles } from "@material-ui/core/styles";
+import SpeedDial from "@material-ui/lab/SpeedDial";
+import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
+import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
+import FileCopyIcon from "@material-ui/icons/FileCopyOutlined";
+import SaveIcon from "@material-ui/icons/Save";
+import PrintIcon from "@material-ui/icons/Print";
+import ShareIcon from "@material-ui/icons/Share";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import EditIcon from "@material-ui/icons/Edit";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: 380,
+    transform: "translateZ(0px)",
+    flexGrow: 1
+  },
+  speedDial: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2)
+  }
+}));
+
+const actions = [
+  { icon: <FileCopyIcon />, name: "Copy" },
+  { icon: <SaveIcon />, name: "Save" },
+  { icon: <PrintIcon />, name: "Print" },
+  { icon: <ShareIcon />, name: "Share" },
+  { icon: <FavoriteIcon />, name: "Like" }
+];
 
 function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div className="App" style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
@@ -10,6 +51,23 @@ function App() {
         defaultZoom={2}
         defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
       ></GoogleMapReact>
+      <SpeedDial
+        ariaLabel="SpeedDial openIcon example"
+        className={classes.speedDial}
+        icon={<SpeedDialIcon openIcon={<EditIcon />} />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={handleClose}
+          />
+        ))}
+      </SpeedDial>
     </div>
   );
 }
